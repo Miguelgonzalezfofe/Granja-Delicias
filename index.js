1735c2bb783b52f9b8d5691d4a25dba2afafce84
Miguel Eduardo Gonzalez Fofe <247mmgg@gmail.com>
2025-08-19 21:55:54 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const form = document.getElementById("form")
-console.log(form)
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("form")
 
-form.addEventListener("submit", (e)=>{
-    e.preventDefault()
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+    })
+
+    
+
 })
+function mensajeWhatsapp(producto) {
+        let mensaje = "Hola quiero realizar un pedido de " + producto
+        window.location.href = `https://wa.me/+541167554915?text=${mensaje}`
+    }
+
+
